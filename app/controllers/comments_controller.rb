cc85cbdaa8f8d05a0b81e7500be6f1eef66bbd06
minh93 <phamducminh.bri@gmail.com>
2015-07-10 09:51:43 +0700

    lastest entries in home

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :find_entry
+  before_action :can_comment
 
   def create
     @entry = Entry.find params[:entry_id]
     @comment = @entry.comments.build comment_param
     if @comment.save
       flash[:success] = "Commented"
-      redirect_to current_user
+      redirect_to @entry.user
     else
       @feed_items = []
       render "static_pages/home"
@@ -17,6 +18,14 @@ class CommentsController < ApplicationController
   def find_entry
     @entry = Entry.find params[:entry_id]
     redirect_to root_path, flash[:warning] = "Access denied" unless @entry
+  end
+
+  def can_comment
+    entry_owner = Entry.find(params[:entry_id]).user
+    if !current_user.followers.include? entry_owner
+      redirect_to current_user
+      flash[:warning] = "Can not create comment here!"
+    end
   end
 
   private
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,13 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in
       @entry = current_user.entries.build
+      @lastest_entries = []
+      followers = current_user.followers
+      followers.each do |f|
+        if !f.entries.last.nil?
+          @lastest_entries<<f.entries.last
+        end
+      end
     end
   end
 
