c7a26c2952d724c0daa7722e5034b16fd0f6513c
minh93 <phamducminh.bri@gmail.com>
2015-07-10 10:33:11 +0700

    finish will paginate for array

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,10 +5,11 @@ class StaticPagesController < ApplicationController
       @lastest_entries = []
       followers = current_user.followers
       followers.each do |f|
-        if !f.entries.last.nil?
-          @lastest_entries<<f.entries.last
+        if !f.entries.first.nil?
+          @lastest_entries<<f.entries.first
         end
       end
+      @lastest_entries = @lastest_entries.sort_by{|entry| entry.created_at}.reverse.paginate(:page => params[:page] ,:per_page => 3)
     end
   end
 
